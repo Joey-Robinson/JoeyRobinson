4232408144178e4d44ae96429cc01d69c99b4a50
elamahpla <theamazingness@gmail.com>
2018-07-31 12:14:48 -0500

    Header work.

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
-    <Link className="Header-Link-Home" to="/">{siteTitle}</Link>
-    <Link className="Header-Link-Skills" to="/skills">Skills</Link>
-    <Link className="Header-Link-About" to="/about">About</Link>
+    <Link activeClassName="Header-Link-Home-Active" exact={true} className="Header-Link-Home" to="/">Home</Link>
+    <Link activeClassName="Header-Link-Skills-Active" className="Header-Link-Skills" to="/skills">Skills</Link>
+    <Link activeClassName="Header-Link-About-Active" className="Header-Link-About" to="/about">About</Link>
   </div>
 )
 
