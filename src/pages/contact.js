fe03ff45e3df188d138979b671dc5aa71211cd95
elamahpla <theamazingness@gmail.com>
2018-07-31 14:37:10 -0500

    Contact.js and _contact.scss done (pre-MQ).

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Contact = () => (
   <div className="Contact">
-    From Contact
+    <div className="Contact-GitHub">
+        <a target="_blank" href="https://github.com/elamahpla">GitHub<br/></a>
+    </div>
+    <div className="Contact-Twitter">
+        <a target="_blank" href="https://twitter.com/joeyrobinsondev">Twitter<br/></a>
+    </div>
+    <div className="Contact-LinkedIn">
+        <a target="_blank" href="https://www.linkedin.com/in/joey-robinson/">LinkedIn<br/></a>
+    </div>
+    <div className="Contact-Email">
+        <a target="_blank" href="mailto:joeyrobinsondev@gmail.com">Email<br/></a>
+    </div>
   </div>
 )
 
