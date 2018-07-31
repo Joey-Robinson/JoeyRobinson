7ea15453e042eaf671ef6c0ddeeb202a9da29d66
elamahpla <theamazingness@gmail.com>
2018-07-31 16:43:32 -0500

    Prepping for header animation.

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,10 @@ import Link from 'gatsby-link'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
-    <Link activeClassName="Header-Link-Home-Active" exact={true} className="Header-Link-Home" to="/">Home</Link>
+    <Link activeClassName="Header-Link-Home-Active" exact={true} className="Header-Link-Home" to="/">
+      <span className="Header-Home-Visible">Home</span>
+      <span className="Header-Home-Invisible">Phone</span>
+    </Link>
     <Link activeClassName="Header-Link-Skills-Active" className="Header-Link-Skills" to="/skills">Skills</Link>
     <Link activeClassName="Header-Link-About-Active" className="Header-Link-About" to="/about">About</Link>
     <Link activeClassName="Header-Link-Portfolio-Active" className="Header-Link-Portfolio" to="/portfolio">Portfolio</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
-const IndexPage = () => (
+const Home = () => (
   <div className="Home">
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hey, I'm Joey</h1>
+    <div className="Home-Welcome">
+      <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I'm a self-taught front-end developer. Through the last year, I've taught myself the basics of web development.
+        While on this continuing journey, I've learned some amazing things, and look forward to learning & knowing more.
+        I can say with certainty that I truly enjoy writing code that is reflected visually for people to enjoy.
+        My hope is that in the future, I can make more things people will like and enjoy.
+      </p>
+    </div>
+    <div className="Home-Middle">
+      <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </p>
+    </div>
   </div>
 )
 
-export default IndexPage
+export default Home
