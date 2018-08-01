53016b71d427dcc3bd5c4d42c33c7f74ad432dfb
elamahpla <theamazingness@gmail.com>
2018-08-01 17:51:00 -0500

    Rotatingcard.js & _rotatingcard.scss.

--- src/components/rotatingcard.js
+++ src/components/rotatingcard.js
@@ -4,12 +4,13 @@ const RotatingCard = () => (
   <div className="RotatingCard">
     <div className="RotatingCard-Side">
       <div className="RotatingCard-Side-Front">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, recusandae!
+        Hello and welcome. Thanks for making the trip here. There's a contact link down there if you have any questions or comments.
       </div>
       <div className="RotatingCard-Picture RotatingCard-Picture-1">
         &nbsp;
       </div>
       <div className="RotatingCard-Side RotatingCard-Side-Back RotatingCard-Side-Back-1">
+        I'm pretty sure all my contact information is there and available, so if you have something to say go ahead.
       </div>  
     </div>
   </div>
