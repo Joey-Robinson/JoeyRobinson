e18a4d7e68e5e591cae59824e7baa203733a7d42
elamahpla <theamazingness@gmail.com>
2018-07-31 19:00:07 -0500

    Blog.js and _blog.scss look done.

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -6,11 +6,10 @@ export default function Template({data}){
   
   return (
     <div className="Blog-Content">
-      <Link className="Blog-Content-GoBack" to="/blog">Go Back</Link>
-      <h5>{post.frontmatter.date}</h5>
+      <Link className="Blog-GoBack" to="/blog">Go Back</Link>
       <h1>{post.frontmatter.title}</h1>
       <div
-        className="Blog-Content-Main" 
+        className="Blog-Main" 
         dangerouslySetInnerHTML={{__html: post.html}} />
         <h4 className="Blog-Content-Maker">
           Posted by: {post.frontmatter.author} on {post.frontmatter.date}
@@ -27,6 +26,7 @@ export const postQuery = graphql`
         path
         title
         author
+        date
       }
     }
   }
