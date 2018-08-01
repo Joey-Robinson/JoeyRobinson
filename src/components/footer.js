b4413ebe7c168673b72867a9c252a42954754480
elamahpla <theamazingness@gmail.com>
2018-07-31 19:20:44 -0500

    Playing with _footer.scss & Footer.js.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => (
   <footer className="Footer">
-    From Footer
+    <div className="Footer-Goodbye">
+      Hey, thanks for stopping by.
+    </div>
+    <div className="Footer-Signature">
+      &copy; Joey Robinson 2018 
+    </div>
   </footer>
 )
 
