e9e3e4eb717b28b1c2a5cac4ed24be1a3ddc5fb1
elamahpla <theamazingness@gmail.com>
2018-08-11 22:59:33 -0500

    changing colors

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,56 +6,6 @@ const NotFoundPage = () => (
     <div className="FourOhFour-Back">
       <Link to="/">You Broke Me, Go Back</Link>
     </div>
-    <div className="FourOhFour-One">&nbsp;</div>
-    <div className="FourOhFour-Two">&nbsp;</div>
-    <div className="FourOhFour-Three">&nbsp;</div>
-    <div className="FourOhFour-Four">&nbsp;</div>
-    <div className="FourOhFour-Five">&nbsp;</div>
-    <div className="FourOhFour-Six">&nbsp;</div>
-    <div className="FourOhFour-Seven">&nbsp;</div>
-    <div className="FourOhFour-Eight">&nbsp;</div>
-    <div className="FourOhFour-Nine">&nbsp;</div>
-    <div className="FourOhFour-Ten">&nbsp;</div>
-    <div className="FourOhFour-Eleven">&nbsp;</div>
-    <div className="FourOhFour-Twelve">&nbsp;</div>
-    <div className="FourOhFour-Thirteen">&nbsp;</div>
-    <div className="FourOhFour-Fourteen">&nbsp;</div>
-    <div className="FourOhFour-Fifteen">&nbsp;</div>
-    <div className="FourOhFour-Sixteen">&nbsp;</div>
-    <div className="FourOhFour-Seventeen">&nbsp;</div>
-    <div className="FourOhFour-Eighteen">&nbsp;</div>
-    <div className="FourOhFour-Nineteen">&nbsp;</div>
-    <div className="FourOhFour-Twenty">&nbsp;</div>
-    <div className="FourOhFour-TwentyOne">&nbsp;</div>
-    <div className="FourOhFour-TwentyTwo">&nbsp;</div>
-    <div className="FourOhFour-TwentyThree">&nbsp;</div>
-    <div className="FourOhFour-TwentyFour">&nbsp;</div>
-    <div className="FourOhFour-TwentyFive">&nbsp;</div>
-    <div className="FourOhFour-TwentySix">&nbsp;</div>
-    <div className="FourOhFour-TwentySeven">&nbsp;</div>
-    <div className="FourOhFour-TwentyEight">&nbsp;</div>
-    <div className="FourOhFour-TwentyNine">&nbsp;</div>
-    <div className="FourOhFour-Thirty">&nbsp;</div>
-    <div className="FourOhFour-ThirtyOne">&nbsp;</div>
-    <div className="FourOhFour-ThirtyTwo">&nbsp;</div>
-    <div className="FourOhFour-ThirtyThree">&nbsp;</div>
-    <div className="FourOhFour-ThirtyFour">&nbsp;</div>
-    <div className="FourOhFour-ThirtyFive">&nbsp;</div>
-    <div className="FourOhFour-ThirtySix">&nbsp;</div>
-    <div className="FourOhFour-ThirtySeven">&nbsp;</div>
-    <div className="FourOhFour-ThirtyEight">&nbsp;</div>
-    <div className="FourOhFour-ThirtyNine">&nbsp;</div>
-    <div className="FourOhFour-Fourty">&nbsp;</div>
-    <div className="FourOhFour-FourtyOne">&nbsp;</div>
-    <div className="FourOhFour-FourtyTwo">&nbsp;</div>
-    <div className="FourOhFour-FourtyThree">&nbsp;</div>
-    <div className="FourOhFour-FourtyFour">&nbsp;</div>
-    <div className="FourOhFour-FourtyFive">&nbsp;</div>
-    <div className="FourOhFour-FourtySix">&nbsp;</div>
-    <div className="FourOhFour-FourtySeven">&nbsp;</div>
-    <div className="FourOhFour-FourtyEight">&nbsp;</div>
-    <div className="FourOhFour-FourtyNine">&nbsp;</div>
-    <div className="FourOhFour-Fifty">&nbsp;</div>
   </div>
 )
 
