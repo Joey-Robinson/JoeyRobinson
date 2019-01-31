63b9a8efff67784a71ea0942dd150dd46f892569
elamahpla <theamazingness@gmail.com>
2019-01-30 23:43:02 -0600

    Header done, looks nice

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,20 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <header className="header">
     <nav className="header-nav">
-      <Link className="header-nav__title" to="/">
-        {siteTitle}
+      <Link className="header-nav__title title" to="/">
+        j
       </Link>
-      <Link className="header-nav__about" to="/about/">
-        About
+      <Link className="header-nav__about about" to="/about/">
+        a
       </Link>
-      <Link className="header-nav__work" to="/work/">
-        Work
+      <Link className="header-nav__work work" to="/work/">
+        w
       </Link>
-      <Link className="header-nav__contact" to="/contact/">
-        Conact
+      <Link className="header-nav__contact contact" to="/contact/">
+        c
       </Link>
     </nav>
   </header>
