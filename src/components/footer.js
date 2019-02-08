29f54fd6202f5f8262ae3e642d1a6d191a3d89ff
Joey Robinson <theamazingness@gmail.com>
2019-02-08 13:07:36 -0600

    updating footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,6 +6,14 @@ const Footer = () => (
     <div className="footer-primary">
       © Joey Robinson, {new Date().getFullYear()}
     </div>
+    <div className="footer-far__left">
+      <ul>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://www.spotify.com/us/">spotify</a></li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://myanimelist.net/">anime</a></li>
+        <li><a target="_blank" rel="noopener noreferrer" href="http://blog.dota2.com/?l=english">dota2</a></li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/">youtube</a></li>
+      </ul>
+    </div>
     <div className="footer-left">
       <ul>
         <li><a target="_blank" rel="noopener noreferrer" href="mailto:joeyrobinsondev@gmail.com">email</a></li>
@@ -36,6 +44,14 @@ const Footer = () => (
             contact
           </Link>
         </li>
+      </ul>
+    </div>
+    <div className="footer-far__right">
+      <ul>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/">mdn</a></li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/">stackoverflow</a></li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://codepen.io/#">codepen</a></li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://codesandbox.io/">codesandbox</a></li>
       </ul>
     </div>
   </footer>
