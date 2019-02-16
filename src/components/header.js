17eb10db960f3b6b6fcf4bf923bf741d80fe6c0a
Joey Robinson <theamazingness@gmail.com>
2019-02-15 22:21:56 -0500

    revamp

--- src/components/header.js
+++ src/components/header.js
@@ -2,30 +2,32 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = () => (
-  <header className="header">
-    <nav className="header-nav nav">
-      <div className="nav-home">
-        <Link to="/">
-          home
-        </Link> 
-      </div>
-      <div className="nav-work">
-        <Link to="/work/">
-          work
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
         </Link>
-      </div>
-      <div className="nav-about">
-        <Link to="/about/">
-          about
-        </Link>
-      </div>
-      <div className="nav-contact">
-        <Link to="/contact/">
-          contact
-        </Link>
-      </div>
-    </nav>
+      </h1>
+    </div>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,20 @@
 import React from "react"
+import { Link } from "gatsby"
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `portfolio`, `react`]} />
-      <div className="home">
-        <div className="home-top">
-          <h1>it's me, joey</h1>
-        </div>
-        <div className="home-left">
-          <p>
-            i try to make cool things with front-end technologies. i like using react to develop projects. i have many interests in life. my primary interest is writing code. i find it not only relaxing but incredibly enjoyable as well.
-          </p>
-          <p>
-            i'm constantly trying to improve on myself as well as my coding capabilities
-          </p>
-        </div>
-        <div className="home-last">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat soluta, ea accusamus ex illo alias!</p>
-        </div>
-      </div>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
