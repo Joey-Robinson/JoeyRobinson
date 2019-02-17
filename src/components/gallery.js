77096ba23422f0fe6ecb625b4607fdd61da43e8e
Joey Robinson <theamazingness@gmail.com>
2019-02-17 01:28:49 -0500

    Final for night

---
+++ src/components/gallery.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import shellgame from '../images/shellgame.png'
+import caloriecounter from '../images/caloriecounter.png'
+import reacttodo from '../images/react-todo.png'
+import mysidia from '../images/mysidia.png'
+import ssc from '../images/ssc.png'
+import tfsoundboard from '../images/tfsoundboard.png'
+import periodictable from '../images/vJS-PeriodicTable.png'
+
+const galleryData = [{
+    title: 'vJS Calorie Counter',
+    image: `${caloriecounter}`,
+    ghRepo: 'https://github.com/Joey-Robinson/CalorieCounter',
+    liveLink: 'https://vjs-calorie-counter.netlify.com/'
+  },
+  {
+    title: 'react todo no hooks',
+    image: `${reacttodo}`,
+    ghRepo: 'https://github.com/Joey-Robinson/react-todo-list-no-hooks',
+    liveLink: 'https://react-todo-no-hooks.netlify.com/'
+  },
+  {
+    title: 'Sweetest Southern Creations',
+    image: `${ssc}`,
+    ghRepo: 'https://github.com/Joey-Robinson/SweetestSouthernCreations/',
+    liveLink: 'sweetestsoutherncreations.com'
+  },
+  {
+    title: '<TF> Soundboard',
+    image: `${tfsoundboard}`,
+    ghRepo: 'https://github.com/Joey-Robinson/tf-soundboard',
+    liveLink: 'https://tf-soundboard.netlify.com/'
+  },
+  {
+    title: 'vJS Periodic Table',
+    image: `${periodictable}`,
+    ghRepo: 'https://github.com/Joey-Robinson/vJS-Periodic-Table',
+    liveLink: 'https://vanillajs-periodic-table.netlify.com/'
+  },
+  {
+    title: 'Mysidia Free Company Website',
+    image: `${mysidia}`,
+    ghRepo: 'https://github.com/Joey-Robinson/MysidiaSite',
+    liveLink: 'https://mysidiafc.netlify.com/#/'
+  },
+  {
+    title: 'Gamer Vanity Site',
+    image: `${shellgame}`,
+    ghRepo: 'https://github.com/Joey-Robinson/secondTribute',
+    liveLink: 'https://meowkats.netlify.com/'
+  },
+  {
+    title: 'Shell Game',
+    image: `${shellgame}`,
+    ghRepo: 'https://github.com/Joey-Robinson/shellgame',
+    liveLink: 'https://joey-robinson.github.io/shellgame/'
+  }
+]
+class Gallery extends Component {
+  render() {
+    return (
+      <ul className="gallery">
+        {galleryData.map((info) => {
+          return <li>
+                  <p>
+                    {info.title} <br />
+                    <a href={info.ghRepo}>GitHub Repo</a>
+                  </p>
+                  <a href={info.liveLink}>
+                  <img src={info.image}/>
+                  </a>
+                </li>
+        })}
+      </ul>
+    )
+  }
+}
+
+export default Gallery
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Gallery from "../components/gallery";
 
 const IndexPage = () => (
   <Layout>
@@ -8,6 +9,7 @@ const IndexPage = () => (
     <section className="home">
       <section className="home__one">
         <h1>from home one</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni assumenda eveniet harum distinctio id quia mollitia necessitatibus numquam consectetur rerum! Quasi accusantium quae officiis minima similique nemo dolore eaque? Quaerat ipsa atque deleniti doloribus error qui aperiam deserunt porro voluptates odit a, fuga aut quibusdam magni illo repellat dolor ipsum.</p>
       </section>
     </section>
     <section className="home__about">
@@ -15,6 +17,7 @@ const IndexPage = () => (
     </section>
     <section className="home__work">
       <h1>from work</h1>
+      <Gallery />
     </section>
   </Layout>
 )
