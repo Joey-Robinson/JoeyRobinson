d2881bdd1109236f737f10177a0a7a6a9590ae9c
Joey Robinson <theamazingness@gmail.com>
2019-02-23 22:23:52 -0500

    ok works, checking one thing

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,6 @@ const IndexPage = () => (
           name="contact-form"
           method="post"
           data-netlify="true"
-          data-netlify-honeypot="bot-field"
         >
           <input name="name" placeholder="Your Name" type="text" />
           <input name="email" placeholder="name@name.com" type="email" />
