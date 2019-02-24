a9b49b90741698d55357ec5f726ca2e1bc8e653b
Joey Robinson <theamazingness@gmail.com>
2019-02-23 22:21:10 -0500

    Ok last one for real

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,19 +46,12 @@ const IndexPage = () => (
           name="contact-form"
           method="post"
           data-netlify="true"
+          data-netlify-honeypot="bot-field"
         >
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email" /></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+          <input name="name" placeholder="Your Name" type="text" />
+          <input name="email" placeholder="name@name.com" type="email" />
+          <textarea name="message" />
+          <button>Send</button>
         </form>
       </section>
     </section>
